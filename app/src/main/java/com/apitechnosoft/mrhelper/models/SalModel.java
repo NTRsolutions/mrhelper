bd949583bba6db99ee64ddeb179c10615568a451
neerajk0702 <neerajk0702@gmail.com>
2018-04-15 13:52:38 +0530

    images

---
+++ app/src/main/java/com/apitechnosoft/mrhelper/models/SalModel.java
@@ -0,0 +1,24 @@
+package com.apitechnosoft.mrhelper.models;
+
+public class SalModel {
+    private String title;
+    private int imageId;
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getImageId() {
+        return imageId;
+    }
+
+    public void setImageId(int imageId) {
+        this.imageId = imageId;
+    }
+
+
+
+}
